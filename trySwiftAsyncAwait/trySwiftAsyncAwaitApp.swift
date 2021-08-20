adff1e297ab41e8c5ceb2d6f737fdc200e050a6d
Mohammad Azam <azamsharp@gmail.com>
2021-08-20 15:27:05 -0500

    Initial Commit

---
+++ trySwiftAsyncAwait/trySwiftAsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  trySwiftAsyncAwaitApp.swift
+//  trySwiftAsyncAwait
+//
+//  Created by Mohammad Azam on 8/20/21.
+//
+
+import SwiftUI
+
+@main
+struct trySwiftAsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
